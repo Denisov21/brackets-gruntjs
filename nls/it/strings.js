bcccbde34a83a26d1b7b7f1930775b6a69239337
Denisov21 <student@alice.it>
2014-08-01 19:12:00 +0200

    Create strings.js
    Create Italian language!

---
+++ nls/it/strings.js
@@ -0,0 +1,18 @@
+/*
+* strings.js
+* @author: Fede91
+* Github: https://github.com/Fede91
+*
+* Made available under a MIT @license:
+* http://www.opensource.org/licenses/mit-license.php
+*
+* strings.js Copyright Fede91 2014.
+*/
+define({
+    BUTTON_CANCEL : "Cancella",
+    BUTTON_SAVE : "Salva",
+    GRUNTJS_SETTINGS: "Impostazioni GruntJS",
+    GRUNTJS_SHORTCUTS : "Scorciatoie",
+    PANEL_SHORTCUT : "Pannello di comando",
+    SHORTCUTS_HINT: "Tasti separati con cruscotto, come questo: Ctrl-Alt-G<br>È possibile utilizzare gli identificatori tasti inglesi Ctrl, Cmd (Mac), Alt e Shift."
+});
